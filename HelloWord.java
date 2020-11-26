3a59377a56040bc88d27b6de5946126ddd7803b8
wangyongtao01 <wangyongtao01@wondersgroup.com>
2020-11-26 09:50:12 +0800

    hello java

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+package com.wonders.businessplatformgateway.support.security;
+
+/**
+ * @ClassNameHelloWord
+ * @Description TODO
+ * @Author Yongtao Wang
+ * @Date2020/11/26 9:48
+ **/
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
